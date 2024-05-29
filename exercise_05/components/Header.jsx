f4edcaedf91bd392186c0ec1a9ffec33b13ae732
redrumkumg <n.ronnapat@github.com>
2024-05-29 09:32:41 +0700

    add more desciption of && condition rendering and ternary

--- exercise_05/components/Header.jsx
+++ exercise_05/components/Header.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from "react";
 
-function Header({temperature, toggleIsOn, isOn}) {
+function Header({ temperature, toggleIsOn, isOn }) {
   return (
     <>
-    <button onClick={toggleIsOn}>ON/OFF</button>
-    {isOn? <span>Current Temperature: {temperature}°C</span>: <span>OFF</span>}
+      <button onClick={toggleIsOn}>ON/OFF</button>
+      { // ใช้ && conditional rendering ได้ในกรณีที่ต้องการคืนค่า true แต่ไม่ต้องใช้ else 
+      /* isOn && (
+      <span className="ml-4 font-bold text-2xl">
+        Current Temperature: {temperature}°C
+      </span>
+      ) */}
+
+      {isOn ? (
+        <span>Current Temperature: {temperature}°C</span>
+      ) : (
+        <span>OFF</span> // ถ้าไม่ต้องใช้ else ใส่ null
+      )}
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
